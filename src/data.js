e824f65b7ab062f3d68dfdfbc38c9c27adb90e0d
elisabethfarfan <elisabethfarfan@gmail.com>
2022-06-24 17:26:50 -0500

    test tyError

--- src/data.js
+++ src/data.js
@@ -28,48 +28,101 @@ export const filterByDate = (arrayObj, año) => {
 };
 
 export const filterDetailsfilms = (array, id) => {
-  const films = array.filter(ob => ob.id.includes(id));
-  return films;
+  try {
+    if(array==null|| id==null || array.length==0||id==""){
+      throw new TypeError("Error")
+    }
+
+
+    const films = array.filter(ob => ob.id.includes(id));
+    return films;
+  } catch (error) {
+    console.toThrow(TypeError);
+  }
+  
 };
 
+
+
 export const repeatYear=(movies)=>{
- const arr=[];
-  movies.forEach((item)=>{
-      if(!arr.includes(item.release_date)){
-        arr.push(item.release_date);
+  try {
+    if(movies==null ||  movies.length==0){
+      throw new TypeError("Error")
     }
-  })
-return arr;
+
+    const arr=[];
+    movies.forEach((item)=>{
+        if(!arr.includes(item.release_date)){
+          arr.push(item.release_date);
+      }
+    })
+  return arr;
+  } catch (error) {
+    console.toThrow(TypeError);
+  }
+
    
 }
+
+
 export const repeatDirector=(movies)=>{
-  const arr=[];
-   movies.forEach((item)=>{
-       if(!arr.includes(item.director)){
-         arr.push(item.director);
-     }
-   })
- return arr;
+
+  try {
+    if(movies==null ||  movies.length==0){
+      throw new TypeError("Error")
+    }
+
+    const arr=[];
+    movies.forEach((item)=>{
+        if(!arr.includes(item.director)){
+          arr.push(item.director);
+      }
+    })
+  return arr;
+  } catch (error) {
+    console.toThrow(TypeError);
+  }
+ 
     
  }
 
  
  export const repeatProducer=(movies)=>{
-  const arr=[];
-   movies.forEach((item)=>{
-       if(!arr.includes(item.producer)){
-         arr.push(item.producer);
-     }
-   })
- return arr;
+  try {
+    if(movies==null ||  movies.length==0){
+      throw new TypeError("Error")
+    }
+
+    const arr=[];
+    movies.forEach((item)=>{
+        if(!arr.includes(item.producer)){
+          arr.push(item.producer);
+      }
+    })
+  return arr;
+  } catch (error) {
+    console.toThrow(TypeError);
+  }
+
     
  }
 
-export const filterByPopular = (arrayObj) => {
-  orderDataScore(arrayObj);
 
-  let films = arrayObj.filter(obj=> obj.rt_score>94);
-return films;
+
+export const filterByPopular = (arrayObj) => {
+
+  try {
+      if(arrayObj==null ||  arrayObj.length==0){
+        throw new TypeError("Error")
+      }
+      orderDataScore(arrayObj);
+
+      let films = arrayObj.filter(obj=> obj.rt_score>94);
+    return films;
+
+    } catch (error) {
+      console.toThrow(TypeError);
+    }
 
 };
 
@@ -84,15 +137,25 @@ return films;
 
 
 export const search = (arrayObj, input) => {
-let arrayNew=[];
-   arrayObj.filter(function(elemento){
-    if(elemento.title.toLowerCase().startsWith(input)){
-      arrayNew.push(elemento);
-      }
-      
+  try {
 
-});
-return arrayNew;
+    if(arrayObj==null|| input==null || arrayObj.length==0||input==""){
+      throw new TypeError("Error")
+    }
+
+
+    let arrayNew=[];
+    arrayObj.filter(function(elemento){
+     if(elemento.title.toLowerCase().startsWith(input)){
+       arrayNew.push(elemento);
+       }
+       
+      });
+ return arrayNew;
+  } catch (error) {
+    console.toThrow(TypeError);
+  }
+
 };
 
 
@@ -107,17 +170,28 @@ export let orderDataScore= (movies) => {
 
 
 export const countDirectorProducer=(movies)=>{
-return movies.reduce((counter, name) => {
-            
-  if (counter[name]) {
-  counter[name] = counter[name] + 1;
-  
-  }else{
-  counter[name] = 1;
-  }
-  return counter
+  try {
+    if(movies==null ||  movies.length==0){
+      throw new TypeError("Error")
+    }
 
-}, {});
+    return movies.reduce((counter, name) => {
+            
+      if (counter[name]) {
+      counter[name] = counter[name] + 1;
+      
+      }else{
+      counter[name] = 1;
+      }
+      return counter
+    
+    }, {});
+    
+  } catch (error) {
+    console.toThrow(TypeError);
+
+  }
+
 
 }
 
