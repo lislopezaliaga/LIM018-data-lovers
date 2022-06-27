907b820856b9c8790674323cf89bbdb0b6586993
elisabethfarfan <elisabethfarfan@gmail.com>
2022-06-27 12:32:30 -0500

    ordenando datos

--- src/data.js
+++ src/data.js
@@ -1,49 +1,35 @@
-
-
-
-/*****************FILTRADO GENERAL***************************/
+/*****************  FILTRADO GENERAL  ***************************/
 export const dataFilterGeneral = (data,category, value) => {
-  try {
-
-    if(data==null|| category==null || data.length==0||category==""||value==null||value==""){
-      throw new TypeError("Error")
-    }
-
-    let result = data.filter(movie => movie[category] === value);
-    return result;
-  } catch (error) {
-    console.toThrow(error);
-  }
- 
-  }
-  /**********************************************************/
-  /*****************ORDENADO GENERAL***************************/
-  
+      try {
+          if(data==null|| category==null || data.length==0||category==""||value==null||value==""){
+            throw new TypeError("Error")
+          }
+          let result = data.filter(movie => movie[category] === value);
+          return result;
+      } catch (error) {
+          console.toThrow(error);
+      }
+}
+ /***************** ORDENADO GENERAL  ***************************/  
   export const orderDataGeneral = (arrayGeneral,category) => {
     try {
-      if(arrayGeneral==null|| category==null || arrayGeneral.length==0||category==""){
-        throw new TypeError("Error")
-      }
-
-      arrayGeneral.sort((a, b) => {
-     
+        if(arrayGeneral==null|| category==null || arrayGeneral.length==0||category==""){
+          throw new TypeError("Error")
+        }
+        arrayGeneral.sort((a, b) => {     
         if (a[category] < b[category]) {
           return -1;
         }
         return 1;
-      })
-    
+      })    
       return arrayGeneral;
-
     } catch (error) {
       console.toThrow(error);
 
-    }
-    
+    }    
   };
 
-
-/**filtra por fecha*/
+ /*****************filtra por fecha**************************/ 
 export const filterByDate = (arrayObj, año) => {
   try {
     if(arrayObj==null|| año==null || arrayObj.length==0||año==""){
@@ -53,34 +39,25 @@ export const filterByDate = (arrayObj, año) => {
     return films;
   } catch (error) {
     console.toThrow(error);
-  }
- 
+  } 
 };
 
-/**filtra detalles de films */
+ /***************** filtra detalles de films **************************/
 export const filterDetailsfilms = (array, id) => {
-  try {
-    if(array==null|| id==null || array.length==0||id==""){
-      throw new TypeError("Error")
-    }
-
-
+  try {  
     const films = array.filter(ob => ob.id.includes(id));
     return films;
   } catch (error) {
     console.toThrow(TypeError);
-  }
-  
+  }  
 };
 
-
-/**NO repetir año */
+ /***************** NO repetir año **************************/
 export const repeatYear=(movies)=>{
   try {
     if(movies==null ||  movies.length==0){
       throw new TypeError("Error")
     }
-
     const arr=[];
     movies.forEach((item)=>{
         if(!arr.includes(item.release_date)){
@@ -90,56 +67,55 @@ export const repeatYear=(movies)=>{
   return arr;
   } catch (error) {
     console.toThrow(TypeError);
-  }
+  }   
+};
 
-   
+ /***************** NO repetir director  **************************/
+export const repeatDirector=(movies)=>{
+    try {
+      if(movies==null ||  movies.length==0){
+        throw new TypeError("Error")
+      }
+      const arr=[];
+      movies.forEach((item)=>{
+          if(!arr.includes(item.director)){
+            arr.push(item.director);
+        }
+      })
+    return arr;
+    } catch (error) {
+      console.toThrow(TypeError);
+    }
+} 
+
+ /*****************NO repetir productor **************************/
+ export const repeatProducer=(movies)=>{
+    try {
+      if(movies==null ||  movies.length==0){
+        throw new TypeError("Error")
+      }
+
+      const arr=[];
+      movies.forEach((item)=>{
+          if(!arr.includes(item.producer)){
+            arr.push(item.producer);
+        }
+      })
+    return arr;
+    } catch (error) {
+      console.toThrow(TypeError);
+    }
 }
 
-/**NO repetir director */
-export const repeatDirector=(movies)=>{
+ /*******************  Ordena por score **************************/
+ let orderDataScore= (movies) => {
+  movies.sort((a, b) => {
+  return b.rt_score-a.rt_score;
+  })
+  return movies;
+};
 
-  try {
-    if(movies==null ||  movies.length==0){
-      throw new TypeError("Error")
-    }
-
-    const arr=[];
-    movies.forEach((item)=>{
-        if(!arr.includes(item.director)){
-          arr.push(item.director);
-      }
-    })
-  return arr;
-  } catch (error) {
-    console.toThrow(TypeError);
-  }
- 
-    
- }
-
- /**NO repetir productor */
- export const repeatProducer=(movies)=>{
-  try {
-    if(movies==null ||  movies.length==0){
-      throw new TypeError("Error")
-    }
-
-    const arr=[];
-    movies.forEach((item)=>{
-        if(!arr.includes(item.producer)){
-          arr.push(item.producer);
-      }
-    })
-  return arr;
-  } catch (error) {
-    console.toThrow(TypeError);
-  }
-
-    
- }
-
-
-/**Filtrar por productor */
+ /******************* Filtrar por productor  **************************/
 export const filterByPopular = (arrayObj) => {
 
   try {
@@ -147,83 +123,50 @@ export const filterByPopular = (arrayObj) => {
         throw new TypeError("Error")
       }
       orderDataScore(arrayObj);
-
       let films = arrayObj.filter(obj=> obj.rt_score>94);
     return films;
-
     } catch (error) {
       console.toThrow(TypeError);
     }
-
 };
 
-
-
-
-
-
-
-
-/**Metodo buscar films */
-
-
+ /******************* Metodo buscar films  **************************/
 export const search = (arrayObj, input) => {
-  try {
+      try {
 
-    if(arrayObj==null|| input==null || arrayObj.length==0||input==""){
-      throw new TypeError("Error")
-    }
-
-
-    let arrayNew=[];
-    arrayObj.filter(function(elemento){
-     if(elemento.title.toLowerCase().startsWith(input)){
-       arrayNew.push(elemento);
-       }
-       
-      });
- return arrayNew;
-  } catch (error) {
-    console.toThrow(TypeError);
-  }
-
-};
-
-/** Ordena por score*/
- let orderDataScore= (movies) => {
-  movies.sort((a, b) => {
-  return b.rt_score-a.rt_score;
-  })
-  return movies;
-
-
-};
-
-/////////////////**Contador de Productores y directores */
-export const countDirectorProducer=(movies)=>{
-  try {
-    if(movies==null ||  movies.length==0){
-      throw new TypeError("Error")
-    }
-
-    return movies.reduce((counter, name) => {
-            
-      if (counter[name]) {
-      counter[name] = counter[name] + 1;
-      
-      }else{
-      counter[name] = 1;
+        if(arrayObj==null|| input==null || arrayObj.length==0||input==""){
+          throw new TypeError("Error")
+        }
+        let arrayNew=[];
+        arrayObj.filter(function(elemento){
+          if(elemento.title.toLowerCase().startsWith(input)){
+            arrayNew.push(elemento);
+            }          
+        });
+    return arrayNew;
+      } catch (error) {
+        console.toThrow(TypeError);
       }
-      return counter
-    
-    }, {});
-    
-  } catch (error) {
-    console.toThrow(TypeError);
-
-  }
+};
 
 
+/*******************  Contador de Productores y directores **************************/
+export const countDirectorProducer=(movies)=>{
+    try {
+      if(movies==null ||  movies.length==0){
+        throw new TypeError("Error")
+      }
+      return movies.reduce((counter, name) => {              
+        if (counter[name]) {
+        counter[name] = counter[name] + 1;        
+        }else{
+        counter[name] = 1;
+        }
+        return counter      
+      }, {});      
+    } catch (error) {
+      console.toThrow(TypeError);
+    }
 }
 
 
--- src/stadistics.js
+++ src/stadistics.js
@@ -5,29 +5,26 @@ export const stadisticsbypopular = (idCanva,labeldata,datagraphic,title) => {
 let graphicMovies=idCanva.getContext('2d');
 var barColors = ["#1ABC9C", "#660099","#2ECC71 ","#660033","#1DB5E2",'#52E21D','#AC1DE2','#282AA0','#A0283F','#2880A0','#A09028','#F390D6','#E5F390','#7A0910','#7CB2B5','#37085F','#365F08','#F0B068','#1DB5E2','#F018F0'];
 //eslint-disable-next-line no-undef
-new Chart(graphicMovies,{
-    type:'bar',
-    data:{
-        labels:labeldata,
-        datasets:[
-            {
-                label: "Most Popular",
-                data: datagraphic,
-                borderColor:'white',  
-                backgroundColor: barColors, 
-                title:'Movies'    
+    new Chart(graphicMovies,{
+        type:'bar',
+        data:{
+            labels:labeldata,
+            datasets:[
+                {
+                    label: "Most Popular",
+                    data: datagraphic,
+                    borderColor:'white',  
+                    backgroundColor: barColors, 
+                    title:'Movies'    
+                }
+                ]},
+        options:{ 
+            plugins:{legend:{position:'bottom',    },
+                title: {
+                    display: true,
+                    text:title,
+                }
             }
-            ]},
-    options:{ 
-        plugins:{legend:{position:'bottom',    },
-        title: {
-            display: true,
-            text:title,
-            
-          }
-
         }
-    }
-    
-
-})   }
+    })
+}
